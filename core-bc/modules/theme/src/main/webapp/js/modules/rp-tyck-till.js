5bfcaded87b1165690447303a87a94684d88f84e
erik.andersson <erik.andersson@monator.com>
2014-04-09 09:29:32 +0000

    Styling of "tyck till" dialog.

--- core-bc/modules/theme/src/main/webapp/js/modules/rp-tyck-till.js
+++ core-bc/modules/theme/src/main/webapp/js/modules/rp-tyck-till.js
@@ -1,145 +1,152 @@
 AUI().add('rp-tyck-till',function(A) {
-    var Lang = A.Lang,
-        
-        DATA_ATTRIBUTE_DIALOG_URL = 'data-dialog-url',
-		DIALOG_HEIGHT = 'dialogHeight',
-		DIALOG_WIDTH = 'dialogWidth',
-        
-        TRIGGER = 'trigger',
-        
-        NAME = 'rp-tyck-till',
-        NS = 'rp-tyck-till',
-        
-        CSS_DIALOG = 'rp-dialog tycktill-dialog',
-        CSS_HIDDEN = 'helper-hidden'
-    ;
-        
-    var TPL_IFRAME = '<div class="iframe-wrap"><iframe src="{iframeSrc}" width="100%" height="100%"></iframe></div>'
-	;    
-        
-    var TyckTill = A.Component.create(
+        var Lang = A.Lang,
+
+            DATA_ATTRIBUTE_DIALOG_URL = 'data-dialog-url',
+            DIALOG_HEIGHT = 'dialogHeight',
+            DIALOG_WIDTH = 'dialogWidth',
+
+            TRIGGER = 'trigger',
+
+            NAME = 'rp-tyck-till',
+            NS = 'rp-tyck-till',
+
+            CSS_DIALOG = 'rp-dialog tycktill-dialog',
+            CSS_HIDDEN = 'helper-hidden'
+            ;
+
+        var TPL_IFRAME = '<div class="iframe-wrap"><iframe src="{iframeSrc}" width="100%" height="100%"></iframe></div>'
+            ;
+
+        var TyckTill = A.Component.create(
             {
                 ATTRS: {
-                	
-    				dialogHeight: {
-    					value: 670
-    				},
-    				
-    				dialogWidth: {
-    					value: 670
-    				},
-                    
+
+                    dialogHeight: {
+                        value: 670
+                    },
+
+                    dialogWidth: {
+                        value: 670
+                    },
+
                     trigger: {
                         value: '#tycktillWrap a',
                         setter: A.one
                     }
-                    
+
                 },
                 EXTENDS: A.Component,
                 NAME: NAME,
                 NS: NS,
-                
+
                 prototype: {
-                    
+
+                    modal: null,
+
                     initializer: function(config) {
                         var instance = this;
-                        
+
                         instance.messages = {};
-                        
-    					instance.messages.dialog = {};
-    					
-    					instance.messages.dialog.title = 'Tyck till';
-    					instance.messages.dialog.close = 'St&auml;ng';
+
+                        instance.messages.dialog = {};
+
+                        instance.messages.dialog.title = 'Tyck till';
+                        instance.messages.dialog.close = 'Stäng';
                     },
-                    
+
                     renderUI: function() {
                         var instance = this;
                     },
-    
+
                     bindUI: function() {
                         var instance = this;
-                        
+
                         instance.get(TRIGGER).on('click', instance._onTriggerClick, instance);
                     },
-                    
+
                     _launchDialog: function(dialogURL) {
-                    	var instance = this;
+                        var instance = this;
 
-						var bodyContent = A.substitute(TPL_IFRAME, {
-							iframeSrc: dialogURL
-						});
-                    	
+                        var bodyContent = A.substitute(TPL_IFRAME, {
+                            iframeSrc: dialogURL
+                        });
 
-                    	var dialog = new A.Modal(
-                             {
-                               bodyContent: bodyContent,
-                               centered: true,
-                               cssClass: CSS_DIALOG,
-                               headerContent: instance.messages.dialog.title,
-                               height: instance.get(DIALOG_HEIGHT),
-                               modal: true,
-                               zIndex: 1000,
-                               width: instance.get(DIALOG_WIDTH)
-                             }
-                           ).render();
-/*
-    					var dialog = new A.Dialog({
-    						bodyContent: bodyContent,
-    						centered: true,
-    						constrain2view: true,
-    						cssClass: CSS_DIALOG,
-    						destroyOnClose: false, //http://issues.liferay.com/browse/AUI-393 setting this to true results in "this.fn is null"
-    						height: instance.get(DIALOG_HEIGHT),
-    						modal: true,
-    						resizable: false,
-    						width: instance.get(DIALOG_WIDTH),
-    						title: instance.messages.dialog.title,
-    						zIndex: 1000,
-    						buttons: [
-    			                        {
-    				                        text: instance.messages.dialog.close,
-    				                        handler: function() {
-    					                        this.close();
-    				                        }
-    			                        }
-    			            ]
-    					}).render();
-*/
-    					dialog.on('render', instance._onDialogRender, instance, dialog);
-                    	
-    					dialog.after('close', function(e) {
-    						var instance = this;
-    						dialog.destroy();
-    					}, instance, dialog);
-                    	
+
+                        instance.modal = new A.Modal(
+                            {
+                                bodyContent: bodyContent,
+                                centered: true,
+                                cssClass: CSS_DIALOG,
+                                headerContent: instance.messages.dialog.title,
+                                height: instance.get(DIALOG_HEIGHT),
+                                modal: true,
+                                zIndex: 1000,
+                                width: instance.get(DIALOG_WIDTH),
+                                toolbars: {
+                                    header: [
+                                        {
+                                            cssClass: 'close',
+                                            label: instance.messages.dialog.close,
+                                            on: {
+                                                click: function(event) {
+                                                    instance.modal.hide();
+                                                    event.domEvent.stopPropagation();
+                                                }
+                                            },
+                                            render: true
+                                        }
+                                    ],
+
+                                    footer: [
+                                        {
+                                            label: instance.messages.dialog.close,
+                                            on: {
+                                                click: function(event) {
+                                                    instance.modal.hide();
+                                                    event.domEvent.stopPropagation();
+                                                }
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        ).render();
+
+                        instance.modal.on('render', instance._onDialogRender, instance, instance.modal);
+
+                        instance.modal.after('hide', function(e) {
+                            var instance = this;
+                            instance.modal.destroy();
+                        }, instance, instance.modal);
+
                     },
-                    
+
                     _onDialogRender: function(e) {
-                    	var instance = this;
+                        var instance = this;
                     },
-                    
+
                     _onTriggerClick: function(e) {
-                    	var instance = this;
+                        var instance = this;
 
-                    	e.halt();
-                    	
-                    	var linkNode = e.currentTarget;
-                    	
-                    	var dialogURL = linkNode.getAttribute(DATA_ATTRIBUTE_DIALOG_URL);
+                        e.halt();
 
-                    	instance._launchDialog(dialogURL);
+                        var linkNode = e.currentTarget;
+
+                        var dialogURL = linkNode.getAttribute(DATA_ATTRIBUTE_DIALOG_URL);
+
+                        instance._launchDialog(dialogURL);
                     },
-                    
+
                     _someFunction: function() {
                         var instance = this;
                     }
 
                 }
             }
-    );
+        );
 
-    A.TyckTill = TyckTill;
-        
+        A.TyckTill = TyckTill;
+
     },1, {
         requires: [
             'aui-base',
@@ -147,6 +154,6 @@ AUI().add('rp-tyck-till',function(A) {
             'aui-loading-mask',
             'aui-modal',
             'substitute'
-      ]
+        ]
     }
 );
